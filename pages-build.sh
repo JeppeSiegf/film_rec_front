0a1411014aac1d3e28e0e0e0b527d775126e733f
JeppeSiegf <10438303@ucn.dk>
2025-11-17 15:06:27 +0100

    bash

---
+++ pages-build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex           
+pwd
+ls -la
+''
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 flutter-sdk
+
+
+export PATH="$PWD/flutter-sdk/bin:$PATH"
+
+nd configure web
+flutter --version
+flutter config --enable-web
+
+
+flutter pub get
+flutter build web --release --web-renderer html
+
+ls -la build/web
